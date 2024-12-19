f56056fcce5703bb2e263ebc1cbeae8a634ed166
anandp803 <anandp803@gmail.com>
2024-12-19 19:42:21 +0530

    4th

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,9 +9,9 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'FinProWeb2';
-  ngOnInit(){
-    let a =0;
-    console.log(a);
-  }
+  title = 'FinProWeb22';
+  // ngOnInit(){
+  //   let a =0;
+  //   console.log(a);
+  // }
 }
